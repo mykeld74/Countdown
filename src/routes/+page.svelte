3e002814ebf82c6a98f779c5fb59c07787f3c862
mykeld74 <mike@msdwebsolutions.com>
2025-10-20 07:22:47 -0600

    update time

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	let minutesLeft;
 	let secondsLeft;
 
-	const endTime = new Date('2025-10-28T15:00:00').getTime();
+	const endTime = new Date('2025-10-28T07:00:00').getTime();
 
 	const countdown = () => {
 		timeLeft = Math.floor((endTime - Date.now()) / 1000);
